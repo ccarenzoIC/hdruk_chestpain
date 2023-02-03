663177559dfabaeffe036213edff65aed32d4ec9
ccarenzoIC <c.carenzo@imperial.ac.uk>
2023-02-03 17:02:28 +0000

    adding files

---
+++ init.bat
@@ -0,0 +1,3 @@
+mamba env update -f ./environment.yml --prune && ^
+conda activate hdruk_cp && ^
+pip install -e .
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='HDRUK Data Science Collaboration on Ches Pain in the NHS.',
+    author='Catalina Carenzo',
+    license='MIT',
+)
